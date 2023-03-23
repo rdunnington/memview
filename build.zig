af9002df37bd6d5265ab3f0224e89dd384fffd4b
Reuben Dunnington <reuben.dunnington@gmail.com>
2023-03-22 22:41:41 -0700

    initial commit, working window and imgui

---
+++ build.zig
@@ -0,0 +1,73 @@
+const std = @import("std");
+const zgui = @import("libs/zgui/build.zig");
+const zglfw = @import("libs/zglfw/build.zig");
+const zgpu = @import("libs/zgpu/build.zig");
+const zpool = @import("libs/zpool/build.zig");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "memview",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const zgui_pkg = zgui.package(b, target, optimize, .{
+        .options = .{ .backend = .glfw_wgpu },
+    });
+
+    // Needed for glfw/wgpu rendering backend
+    const zglfw_pkg = zglfw.package(b, target, optimize, .{});
+    const zpool_pkg = zpool.package(b, target, optimize, .{});
+    const zgpu_pkg = zgpu.package(b, target, optimize, .{
+        .deps = .{ .zpool = zpool_pkg.zpool, .zglfw = zglfw_pkg.zglfw },
+    });
+
+    zgui_pkg.link(exe);
+    zglfw_pkg.link(exe);
+    zgpu_pkg.link(exe);
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    exe.install();
+
+    // This *creates* a RunStep in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = exe.run();
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing.
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&exe_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,100 @@
+const std = @import("std");
+const zgui = @import("zgui");
+const zglfw = @import("zglfw");
+const zgpu = @import("zgpu");
+
+const AppContext = struct {
+    allocator: std.mem.Allocator,
+    gctx: *zgpu.GraphicsContext,
+};
+
+fn update(app: *AppContext) void {
+    zgui.setNextWindowPos(.{ .x = 0.0, .y = 0.0, .cond = .always });
+    zgui.setNextWindowSize(.{ .w = @intToFloat(f32, app.gctx.swapchain_descriptor.width), .h = -1.0, .cond = .always });
+
+    const title_bar_flags = zgui.WindowFlags{
+        .no_title_bar = true,
+        .no_resize = true,
+        .no_move = true,
+    };
+    if (zgui.begin("menu_bar", .{ .flags = title_bar_flags })) {
+        zgui.textUnformattedColored(.{ 0, 0.8, 0, 1 }, "Average :");
+        zgui.sameLine(.{});
+        zgui.text(
+            "{d:.3} ms/frame ({d:.1} fps)",
+            .{ app.gctx.stats.average_cpu_time, app.gctx.stats.fps },
+        );
+        zgui.end();
+    }
+}
+
+fn draw(app: *AppContext) void {
+    const swapchain_texv = app.gctx.swapchain.getCurrentTextureView();
+    defer swapchain_texv.release();
+
+    const commands = commands: {
+        const encoder = app.gctx.device.createCommandEncoder(null);
+        defer encoder.release();
+
+        // Gui pass.
+        {
+            const pass = zgpu.beginRenderPassSimple(encoder, .load, swapchain_texv, null, null, null);
+            defer zgpu.endReleasePass(pass);
+            zgui.backend.draw(pass);
+        }
+
+        break :commands encoder.finish(null);
+    };
+    defer commands.release();
+
+    app.gctx.submit(&.{commands});
+    _ = app.gctx.present();
+}
+
+pub fn main() !void {
+    zglfw.init() catch {
+        std.log.err("Failed to initialize GLFW library.", .{});
+        return;
+    };
+    defer zglfw.terminate();
+
+    const window = zglfw.Window.create(1280, 720, "memview", null) catch {
+        std.log.err("Failed to create demo window.", .{});
+        return;
+    };
+    defer window.destroy();
+    window.setSizeLimits(400, 400, -1, -1);
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var app = AppContext{
+        .allocator = allocator,
+        .gctx = try zgpu.GraphicsContext.create(allocator, window),
+    };
+
+    defer app.gctx.destroy(allocator);
+
+    zgui.init(allocator);
+    defer zgui.deinit();
+
+    zgui.backend.initWithConfig(
+        window,
+        app.gctx.device,
+        @enumToInt(zgpu.GraphicsContext.swapchain_format),
+        .{ .texture_filter_mode = .linear, .pipeline_multisample_count = 1 },
+    );
+    defer zgui.backend.deinit();
+
+    while (!window.shouldClose() and window.getKey(.escape) != .press) {
+        zglfw.pollEvents();
+        zgui.backend.newFrame(
+            app.gctx.swapchain_descriptor.width,
+            app.gctx.swapchain_descriptor.height,
+        );
+
+        update(&app);
+        draw(&app);
+    }
+}
